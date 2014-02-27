bad259c0748bb738d3c98878314fc8a9e00b6ff1
Diego Hernandes <diego@he.rnand.es>
2014-02-27 11:25:57 -0300

    topicos traduzidos - teste acentos

--- app/database/seeds/TagSeeder.php
+++ app/database/seeds/TagSeeder.php
@@ -14,28 +14,28 @@ class TagSeeder extends Seeder
     private function createTags()
     {
         $commonTags = [
-            'Installation' => 'framework installation, package installation, application installation',
-            'Configuration' => 'framework configuration, web-server configuration, application configuration',
-            'Authentication' => 'topics related to authentication, including user logins, oauth, etc',
-            'Security' => 'code safety, user roles and authorization',
-            'Requests' => 'information related to handling requests',
-            'Input' => 'handling user input',
-            'Session' => 'persisting data between requests using PHP or Laravel sessions',
+            'Instalação' => 'instalacao do framework, instalacao do pacotes, instalacao de aplicacao',
+            'Configuração' => 'framework configuration, web-server configuration, application configuration',
+            'Autenticação' => 'topics related to authentication, including user logins, oauth, etc',
+            'Segurança' => 'code safety, user roles and authorization',
+            'Pedidos' => 'information related to handling requests',
+            'Entrada' => 'handling user input',
+            'Sessão' => 'persisting data between requests using PHP or Laravel sessions',
             'Cache' => 'performance caching or any use of Laravel\'s cache system',
-            'Database' => 'query-building, connections, or drivers',
+            'Batabase' => 'query-building, connections, or drivers',
             'Eloquent' => 'Eloquent modeling, relationships, etc',
             'Ioc' => 'binding to and resolving from the IoC container',
             'Views' => 'topics related to the rendering of views',
             'Blade' => 'topics related to Blade templating',
-            'Forms' => 'topics related to forms',
-            'Validation' => 'topics related to the validation of data',
-            'Mail' => 'topics related to compiling and sending email',
+            'Formularios' => 'topics related to forms',
+            'Validação' => 'topics related to the validation of data',
+            'Email' => 'topics related to compiling and sending email',
             'Queues' => 'topics related to queues',
-            'LaravelIO' => 'topics that relate to the Laravel.io site or community',
-            'Packages' => 'topics related to creating, discussing, and importing packages',
-            'Meetups' => 'topics related to community meetups or user groups',
-            'OOP' => 'topics related to writing good object-oriented code',
-            'Testing' => 'topics related to automated testing',
+            'Laravel.com.br' => 'topics that relate to the Laravel.com.br site or community',
+            'Pacotes' => 'topics related to creating, discussing, and importing packages',
+            'Encontros' => 'topics related to community meetups or user groups',
+            'POO' => 'topics related to writing good object-oriented code',
+            'Testes' => 'topics related to automated testing',
         ];
 
         foreach ($commonTags as $name => $description) {
@@ -49,7 +49,7 @@ class TagSeeder extends Seeder
         }
 
         $articleTags = [
-            'News' => 'information about an occurance',
+            'Noticias' => 'information about an occurance',
         ];
 
         foreach ($articleTags as $name => $description) {
