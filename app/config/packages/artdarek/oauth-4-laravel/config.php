1ce532c068945c114eb605cff82f7b7a1681740d
Diego Hernandes <diego@he.rnand.es>
2014-03-05 12:20:44 -0300

    db and github as apache envs

--- app/config/packages/artdarek/oauth-4-laravel/config.php
+++ app/config/packages/artdarek/oauth-4-laravel/config.php
@@ -5,8 +5,8 @@ return [
 
     'consumers' => [
         'GitHub' => [
-            'client_id'     => '',
-            'client_secret' => '',
+            'client_id'     => apache_getenv('github_id'),
+            'client_secret' => apache_getenv('github_secret'),
             'scope'         => ['user'],
         ],
     ],
